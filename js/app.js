0011afda28e31deabcfdedf10ac3dcdb055e709c
Lucas Shuras <yurastantelucas@gmail.com>
2022-09-30 02:06:15 -0300

    Edited js and index

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const myDoc = document;
+
+console.log(myDoc);
+
+let myH1 = myDoc.getElementsByTagName("h1");
+let myH3 = myDoc.getElementById("mySuperH3");
+
+console.log(myH1);
+console.log(myH3);
+
+myDoc.get
+
+
+const myObj = {
+    "atributo" : "valor";
+}
